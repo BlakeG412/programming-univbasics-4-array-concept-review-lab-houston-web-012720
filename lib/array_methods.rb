3b65a6d1b1d50ec0f28722093c43d0e3db522998
Blake Gaal <bag.blakeandrewgaal@gmail.com>
2020-01-12 01:03:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,8 +12,8 @@ end
 def find_max_value(array)
   x=array[0]
   array.length.times |index|
-  if array[counter] > x 
-    x = array[counter]
+  if array[index] > x 
+    x = array[index]
   end 
   x
 end
